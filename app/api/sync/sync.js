71f20d013e4749954ddc64492accc3eef2b7bab3
Pavanaprana Dasa <pavamana1123@gmail.com>
2023-06-27 13:11:04 +0530

    ok

--- app/api/sync/sync.js
+++ app/api/sync/sync.js
@@ -1,167 +1,157 @@
+const axios = require('axios')
+const mysql = require('mysql')
+const fs = require('fs')
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "iskconmy_folk"
+})
+
+connection.connect((err)=>{
+  if(err){
+    throw err
+  }
+})
 
 function generateInsertStatements(tableName, entries) {
-  const insertStatements = [];
+  const insertStatements = []
 
   for (const entry of entries) {
-    const columns = [];
-    const values = [];
+    const columns = []
+    const values = []
 
     // Handle exceptions for the "participants" table
     if (tableName === 'participants') {
-      const username = entry.name.toLowerCase().replace(/\s/g, '');
-      entry.buddy = entry.buddy === 'Core' || entry.buddy === '-' ? null : entry.buddy.toLowerCase().replace(/\s/g, '');
-      const pass = '';
-      entry.preacher = entry.preacher.toLowerCase().replace(/\s/g, '');
+      const username = entry.name.toLowerCase().replace(/\s/g, '')
+      entry.buddy = entry.buddy === 'Core' || entry.buddy === '-' ? null : entry.buddy.toLowerCase().replace(/\s/g, '')
+      const pass = ''
+      entry.preacher = entry.preacher.toLowerCase().replace(/\s/g, '')
 
-      columns.push(`${tableName}.username`, `${tableName}.pass`);
-      values.push(username, pass);
+      columns.push(`${tableName}.username`, `${tableName}.pass`)
+      values.push(username, pass)
     }
 
     // Handle exceptions for the "registrations" table
     if (tableName === 'registrations') {
-      entry.name = entry.name === '#N/A' ? null : entry.name;
-      const username = entry.name === null ? null : entry.name.toLowerCase().replace(/\s/g, '');
+      entry.name = entry.name === '#N/A' ? null : entry.name
+      const username = entry.name === null ? null : entry.name.toLowerCase().replace(/\s/g, '')
 
-      columns.push(`${tableName}.username`);
-      values.push(username);
+      columns.push(`${tableName}.username`)
+      values.push(username)
     }
 
     // Handle exceptions for the "participation" table
     if (tableName === 'participation') {
-      entry.name = entry.name === '#N/A' ? null : entry.name;
-      entry.remarks = Buffer.from(entry.remarks).toString('base64');
-      const username = entry.name === null ? null : entry.name.toLowerCase().replace(/\s/g, '');
-      entry.caller = (entry.caller||"").toLowerCase().replace(/\s/g, '');
+      entry.name = entry.name === '#N/A' ? null : entry.name
+      entry.remarks = Buffer.from(entry.remarks).toString('base64')
+      const username = entry.name === null ? null : entry.name.toLowerCase().replace(/\s/g, '')
+      entry.caller = (entry.caller||"").toLowerCase().replace(/\s/g, '')
 
-      columns.push(`${tableName}.username`);  
-      values.push(username);
+      columns.push(`${tableName}.username`)  
+      values.push(username)
     } 
 
     for (const column in entry) {
-      columns.push(`${tableName}.${column}`);
-      values.push(entry[column]);
+      columns.push(`${tableName}.${column}`)
+      values.push(entry[column])
     }
 
-    const columnString = columns.join(', ');
+    const columnString = columns.join(', ')
     const valueString = values.map(value => {
       if (typeof value === 'string') {
-        return `'${value.replace(/'/g, "''")}'`;
+        return `'${value.replace(/'/g, "''")}'`
       }else if(value===null){
         return "null"
       }
-      return value;
-    }).join(', ');
+      return value
+    }).join(', ')
 
-    const insertStatement = `INSERT INTO ${tableName} (${columnString}) VALUES (${valueString});`;
-    insertStatements.push(insertStatement);
+    const insertStatement = `INSERT INTO ${tableName} (${columnString}) VALUES (${valueString})`
+    insertStatements.push(insertStatement)
   }
 
-  return insertStatements;
+  return insertStatements
 }
 
 function generateDeleteQueries(data) {
-  const deleteQueries = [];
+  const deleteQueries = []
 
   for (const tableName in data) {
-    const deleteQuery = `DELETE FROM iskconmy_folk.${tableName};`;
-    deleteQueries.push(deleteQuery);
+    const deleteQuery = `DELETE FROM iskconmy_folk.${tableName}`
+    deleteQueries.push(deleteQuery)
   }
 
-  return deleteQueries;
+  return deleteQueries
 }
-  
-const axios = require('axios');
 
 async function makePostRequest() {
   try {
-    const url = 'https://vol.iskconmysore.org/api';
-    const body = { func: 'syncFOLK' };
+    const url = 'https://vol.iskconmysore.org/api'
+    const body = { func: 'syncFOLK' }
 
-    const response = await axios.post(url, body);
-    return response.data;
+    console.log(new Date(), `Fetching data`)
+    const response = await axios.post(url, body)
+    console.log(new Date(), `Data received`)
+    return response.data
   } catch (error) {
-    console.error('An error occurred:', error.response.data);
-    throw error;
+    console.error('An error occurred:', error.response.data)
+    throw error
   }
 }
 
-const mysql = require('mysql');
-
-function executeMultipleStatementQuery(query) {
+function executeQuery(query) {
   return new Promise((resolve, reject) => {
-    // Create a connection to the MySQL database
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "iskconmy_folk"
-    });
-
-    // Connect to the database
-    connection.connect((err) => {
+    connection.query(query, (err, results, fields) => {
       if (err) {
-        reject(err);
-        return;
+        reject(err)
+        return
       }
-
-      console.log('Connected to the database');
-
-      // Execute the multiple statement query
-      connection.query(query, (err, results, fields) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-
-        // Close the database connection
-        connection.end((err) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          console.log('Database connection closed');
-
-          resolve(results);
-        });
-      });
-    });
-  });
+      resolve(results)
+    })
+  })
 }
-
 
 // Example usage
 makePostRequest()
-  .then(apiResponse => {
+  .then(async apiResponse => {
     var insertStatements = []
     var tables = apiResponse.data
     for (const tableName in tables) {
-        const tableEntries = tables[tableName];
+        const tableEntries = tables[tableName]
         insertStatements = insertStatements.concat(generateInsertStatements(tableName, tableEntries))
     }
 
     var delQ = generateDeleteQueries(tables)
     insertStatements = delQ.concat(insertStatements)
 
-    const fs = require('fs');
+    var errors = []
 
-    try {
-    fs.writeFileSync('sync.sql', insertStatements.join(`
-`));
-        console.log('String successfully written to sync.sql');
-    } catch (err) {
-        console.error('Error writing to file:', err);
+    console.log(new Date(), `Sync began`)
+    for(var i=0; i<insertStatements.length; i++){
+      let s = insertStatements[i]
+      try {
+        await executeQuery(s)
+      } catch(e){
+        errors.push({
+          query: s,
+          error: e
+        })
+      }
+    }
+    console.log(new Date(), `Sync completed`)
+
+    if(errors.length){
+      console.log(`Errors: ${JSON.stringify(errors, null, 2)}`)
     }
 
-//     executeMultipleStatementQuery(insertStatements.join(`
-// `))
-//     .then(results => {
-//         console.log('Sync complete');
-//     })
-//     .catch(error => {
-//         console.error('Error executing the query:', error);
-//     });
+    connection.end((err) => {
+      if (err) {
+        throw err
+      }
+    })
   })
   .catch(error => {
-    console.error('Something went wrong:', error);
-  });
+    console.error('Something went wrong:', error)
+  })
--- data/struct.sql
+++ data/struct.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `iskconmy_folk` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `iskconmy_folk` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `iskconmy_folk`;
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
@@ -25,14 +25,14 @@ DROP TABLE IF EXISTS `calendar`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `calendar` (
-  `eventId` varchar(45) NOT NULL,
-  `date` varchar(45) DEFAULT NULL,
-  `program` varchar(45) DEFAULT NULL,
-  `name` mediumtext,
-  `venue` varchar(45) DEFAULT NULL,
-  `speaker` varchar(45) DEFAULT NULL,
+  `eventId` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `date` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `program` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` mediumtext COLLATE utf8mb4_general_ci,
+  `venue` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `speaker` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`eventId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,17 +43,17 @@ DROP TABLE IF EXISTS `compute`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `compute` (
-  `username` varchar(255) NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `attendance` int DEFAULT NULL,
   `sosAttendance` int DEFAULT NULL,
   `folk2Attendance` int DEFAULT NULL,
   `folk4Attendance` int DEFAULT NULL,
   `lastSeen` int DEFAULT NULL,
   `regularity` float DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
+  `status` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `activeness` float DEFAULT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,36 +64,36 @@ DROP TABLE IF EXISTS `participants`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `participants` (
-  `username` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `phone` varchar(45) NOT NULL,
-  `program` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `dob` varchar(45) DEFAULT NULL,
-  `snoozeDate` varchar(45) DEFAULT NULL,
+  `username` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `program` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `dob` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `snoozeDate` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `isStudent` tinyint DEFAULT NULL,
   `callNotAvailable` tinyint DEFAULT '0',
   `whatsAppNotAvailable` tinyint DEFAULT '0',
-  `yearOfJoining` varchar(45) DEFAULT NULL,
-  `institution` mediumtext,
-  `course` mediumtext,
-  `company` mediumtext,
-  `highestQualification` mediumtext,
-  `designation` mediumtext,
-  `tShirtSize` varchar(45) DEFAULT NULL,
-  `skills` longtext,
-  `comments` longtext,
-  `dateAdded` varchar(45) DEFAULT NULL,
-  `addedBy` varchar(45) DEFAULT NULL,
-  `preacher` varchar(45) DEFAULT NULL,
-  `buddy` varchar(45) DEFAULT NULL,
-  `source` varchar(45) DEFAULT NULL,
-  `category` varchar(45) DEFAULT 'General',
-  `batch` varchar(45) DEFAULT NULL,
-  `native` mediumtext,
-  `pass` varchar(45) DEFAULT NULL,
+  `yearOfJoining` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `institution` mediumtext COLLATE utf8mb4_general_ci,
+  `course` mediumtext COLLATE utf8mb4_general_ci,
+  `company` mediumtext COLLATE utf8mb4_general_ci,
+  `highestQualification` mediumtext COLLATE utf8mb4_general_ci,
+  `designation` mediumtext COLLATE utf8mb4_general_ci,
+  `tShirtSize` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `skills` longtext COLLATE utf8mb4_general_ci,
+  `comments` longtext COLLATE utf8mb4_general_ci,
+  `dateAdded` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `addedBy` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `preacher` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `buddy` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `source` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `category` varchar(45) COLLATE utf8mb4_general_ci DEFAULT 'General',
+  `batch` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `native` mediumtext COLLATE utf8mb4_general_ci,
+  `pass` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,17 +104,17 @@ DROP TABLE IF EXISTS `participation`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `participation` (
-  `eventId` varchar(45) NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `phone` varchar(45) DEFAULT NULL,
-  `caller` varchar(45) DEFAULT NULL,
-  `response` varchar(45) DEFAULT NULL,
-  `remarks` longtext,
+  `eventId` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `caller` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `response` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `remarks` longtext COLLATE utf8mb4_general_ci,
   `attendance` tinyint DEFAULT NULL,
-  `time` varchar(45) DEFAULT NULL,
+  `time` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   UNIQUE KEY `uc_compound_key` (`eventId`,`name`,`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,12 +125,12 @@ DROP TABLE IF EXISTS `programs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `programs` (
-  `id` varchar(45) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
+  `id` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `type` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `level` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -141,27 +141,27 @@ DROP TABLE IF EXISTS `prospects`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prospects` (
-  `id` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `phone` varchar(45) NOT NULL,
-  `program` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
+  `id` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `program` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `isStudent` tinyint DEFAULT NULL,
   `callNotAvailable` tinyint DEFAULT '0',
   `whatsAppNotAvailable` tinyint DEFAULT '0',
   `yearOfJoining` int DEFAULT NULL,
-  `institution` varchar(45) DEFAULT NULL,
-  `course` varchar(45) DEFAULT NULL,
-  `company` varchar(45) DEFAULT NULL,
-  `highestQualification` varchar(45) DEFAULT NULL,
-  `designation` varchar(45) DEFAULT NULL,
-  `skills` varchar(500) DEFAULT NULL,
-  `comments` varchar(500) DEFAULT NULL,
-  `dateAdded` varchar(45) DEFAULT NULL,
-  `addedBy` varchar(45) DEFAULT NULL,
-  `source` varchar(45) DEFAULT NULL,
-  `batch` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `institution` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `course` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `company` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `highestQualification` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `designation` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `skills` varchar(500) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `comments` varchar(500) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `dateAdded` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `addedBy` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `source` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `batch` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -172,17 +172,17 @@ DROP TABLE IF EXISTS `registrations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `registrations` (
-  `date` varchar(45) NOT NULL,
-  `program` varchar(45) DEFAULT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `phone` varchar(45) DEFAULT NULL,
+  `date` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `program` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `username` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `paid` tinyint DEFAULT NULL,
-  `paymentMode` varchar(45) DEFAULT NULL,
-  `paymentReference` varchar(45) DEFAULT NULL,
+  `paymentMode` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `paymentReference` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `meta` json DEFAULT NULL,
   UNIQUE KEY `rc_compound_key` (`program`,`name`,`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -193,12 +193,12 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `username` varchar(45) NOT NULL,
-  `roleIndex` varchar(45) DEFAULT NULL,
-  `roleID` varchar(45) DEFAULT NULL,
-  `roleName` varchar(45) DEFAULT NULL,
+  `username` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `roleIndex` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `roleID` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `roleName` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,13 +209,13 @@ DROP TABLE IF EXISTS `sessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sessions` (
-  `program` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `message` varchar(45) DEFAULT NULL,
-  `canvaLink` mediumtext,
-  `posterLink` varchar(45) DEFAULT NULL,
+  `program` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `message` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `canvaLink` mediumtext COLLATE utf8mb4_general_ci,
+  `posterLink` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   UNIQUE KEY `sc_compound_key` (`program`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -226,15 +226,15 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `username` varchar(45) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `phone` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `addedBy` varchar(45) DEFAULT NULL,
-  `addedDate` varchar(45) DEFAULT NULL,
-  `pass` varchar(45) DEFAULT NULL,
+  `username` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `addedBy` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `addedDate` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `pass` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -246,4 +246,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-13 16:08:00
+-- Dump completed on 2023-06-27 13:04:16
